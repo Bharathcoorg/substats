6be674e119660c826c00dd2e72bca8fb51d18296
Bharathcoorg <40591495+Bharathcoorg@users.noreply.github.com>
2023-09-25 02:32:59 +0530

    Update index.js

--- app/sync-block/index.js
+++ app/sync-block/index.js
@@ -50,7 +50,7 @@ async function main() {
   if (tmp.length > 0) {
     currHeight = tmp[0].blockHeight + 1;
   }
-  currHeight = 10000000;
+  currHeight = 15000000;
   console.log("currHeight", currHeight);
   if (maxHeight < currHeight) {
     maxHeight = currHeight + 1;
